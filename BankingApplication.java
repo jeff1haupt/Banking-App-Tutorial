66f5e3e881aeabf19d87c1d60e8874dba8dd8990
Jeff Haupt <66330433+jeff1haupt@users.noreply.github.com>
2020-08-02 20:31:07 -0400

    Updated
    Still need to clean up logic and build interface

--- BankingApplication.java
+++ BankingApplication.java
@@ -5,7 +5,8 @@ public class BankingApplication {
 
 	public static void main(String[] args) {
 		
-		
+		BankAccount obj1 = new BankAccount("TEST", "0004352");
+		obj1.showMenu();
 		
 	}
 	
@@ -37,7 +38,7 @@ class BankAccount {
 		}
 	}
 	
-	void getPreviousTranasction() {
+	void getPreviousTransaction() {
 		if ( previousTransaction > 0 ) {
 			System.out.println("Deposited: " + previousTransaction);
 		} else if ( previousTransaction < 0 ) {
@@ -59,5 +60,58 @@ class BankAccount {
 		System.out.println("C. Withdraw");
 		System.out.println("D. Previous Transaction");
 		System.out.println("E. Exit");
+		
+		do {
+			System.out.println("============================================================");
+			System.out.println("Enter an option");
+			System.out.println("============================================================");
+			option = scanner.next().charAt(0);
+			System.out.println("\n");
+			
+			switch(option) {
+			
+			case 'A':
+				System.out.println("--------------------------------");
+				System.out.println("Balance = " + balance);
+				System.out.println("--------------------------------");
+				System.out.println("\n");
+				break;
+			
+			case 'B':
+				System.out.println("--------------------------------");
+				System.out.println("Enter an amount to deposit: ");
+				System.out.println("--------------------------------");
+				int amount = scanner.nextInt();
+				deposit(amount);
+				System.out.println("\n");
+				break;
+				
+			case 'C':
+				System.out.println("--------------------------------");
+				System.out.println("Enter an amount to withdraw: ");
+				System.out.println("--------------------------------");
+				int amount2 = scanner.nextInt();
+				withdraw(amount2);
+				System.out.println("\n");
+				break;
+				
+			case 'D':
+				System.out.println("--------------------------------");
+				getPreviousTransaction();
+				System.out.println("--------------------------------");
+				System.out.println("\n");
+				break;
+				
+			case 'E':
+				System.out.println("*****************************************");
+				break;
+				
+			default:
+				System.out.println("Invalid option...Please enter again");
+				break;
+			}
+		} while ( option != 'E' ); 
+		
+		System.out.println("Thank you for using MegaBank!");
 	}
 }
