0ffd77df36b52b4e6b894532d8315709a3e5cf4f
Jeff Haupt <66330433+jeff1haupt@users.noreply.github.com>
2020-08-04 19:06:32 -0400

    Add files via upload

--- BankingApplication.java
+++ BankingApplication.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import javax.swing.*;
+
 
 public class BankingApplication {
 
@@ -7,11 +9,17 @@ public class BankingApplication {
 		
 		Scanner s = new Scanner(System.in); 
 		
+		JFrame f;
+	    f=new JFrame();   
+	    String userName = JOptionPane.showInputDialog(f,"Enter User Name: "); 
+	    String accountNum = JOptionPane.showInputDialog(f,"Enter Pin: "); 
+	    
+		/*
 		System.out.println("Enter your user name:\n ");
 		String userName = s.nextLine();
 		System.out.println("Enter your pin number: \n");
 		String accountNum = s.nextLine();
-		
+		*/ 
 		
 		BankAccount obj1 = new BankAccount(userName, accountNum);
 		obj1.showMenu();
